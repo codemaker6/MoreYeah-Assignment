b304ee83348aa064d398929214364b9db2978534
AmanSynkrama <aman@synkrama.com>
2021-10-09 18:49:39 +0530

    added srvices block

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,7 +1,46 @@
+import food from "../assets/Images/foodmineral.png";
+import delivery from "../assets/Images/delivery.png";
+import packing from "../assets/Images/package.png";
 const Services = () => {
   return (
     <>
-      <h1>hello servuices</h1>
+      <div className="serviceBlock">
+        <div>
+          <div className="head">
+            <h1 className="serviceHeading">Our services for You</h1>
+            <p className="serviceTitle">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              <br /> eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <div className="services">
+            <div>
+              <img className="serviceImage" src={food} alt="food mineral" />
+              <h5 className="serviceName">Vitamin and minerals</h5>
+              <p className="serviceDetail">
+                HuLorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt
+              </p>
+            </div>
+            <div>
+              <img className="serviceImage" src={delivery} alt="food mineral" />
+              <h5 className="serviceName">On time delivery</h5>
+              <p className="serviceDetail">
+                HuLorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt
+              </p>
+            </div>
+            <div>
+              <img className="serviceImage" src={packing} alt="food mineral" />
+              <h5 className="serviceName">Secured Packaging</h5>
+              <p className="serviceDetail">
+                HuLorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
